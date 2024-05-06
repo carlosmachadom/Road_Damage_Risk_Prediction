6ba9840787555ffe10ecb65021d28cede20a7f8f
Jau2002 <idamendjaume@gmail.com>
2024-05-06 02:36:50 -0500

    lectura de archivo en persistencia, expecificacion DTO y DAO ademas ejemplo en el controlador

--- src/co/edu/unbosque/controller/Controller.java
+++ src/co/edu/unbosque/controller/Controller.java
@@ -1,5 +1,37 @@
 package co.edu.unbosque.controller;
 
+import co.edu.unbosque.DAO.FuzzyVariablesDAO;
+import co.edu.unbosque.DTO.FuzzyInputDTO;
+
 public class Controller {
-    public Controller() {}
+
+    private final FuzzyVariablesDAO fuzzyVariablesDAO;
+
+    public Controller() {
+        fuzzyVariablesDAO = new FuzzyVariablesDAO();
+        run();
+    }
+
+    private void run() {
+        fuzzyVariablesDAO.putFuzzifiedVariable(new FuzzyInputDTO("trafico", 100.3));
+        fuzzyVariablesDAO.putFuzzifiedVariable(new FuzzyInputDTO("peso_vehiculos", .5));
+        fuzzyVariablesDAO.putFuzzifiedVariable(new FuzzyInputDTO("humedad", .2));
+        fuzzyVariablesDAO.putFuzzifiedVariable(new FuzzyInputDTO("precipitacion", .1));
+        fuzzyVariablesDAO.putFuzzifiedVariable(new FuzzyInputDTO("riesgo_de_alto_deterioro", .9));
+
+        fuzzyVariablesDAO.evaluate();
+
+        FuzzyInputDTO f = fuzzyVariablesDAO.getDefuzzifiedVariable("trafico");
+        FuzzyInputDTO g = fuzzyVariablesDAO.getDefuzzifiedVariable("peso_vehiculos");
+        FuzzyInputDTO h = fuzzyVariablesDAO.getDefuzzifiedVariable("humedad");
+        FuzzyInputDTO i = fuzzyVariablesDAO.getDefuzzifiedVariable("precipitacion");
+        FuzzyInputDTO j = fuzzyVariablesDAO.getDefuzzifiedVariable("riesgo_de_alto_deterioro");
+
+        System.out.println(f.getkey() + " <=> " + f.getValue());
+        System.out.println(g.getkey() + " <=> " + g.getValue());
+        System.out.println(h.getkey() + " <=> " + h.getValue());
+        System.out.println(i.getkey() + " <=> " + i.getValue());
+        System.out.println(j.getkey() + " <=> " + j.getValue());
+    }
+
 }
--- src/co/edu/unbosque/model/persistence/FclFileManager.java
+++ src/co/edu/unbosque/model/persistence/FclFileManager.java
@@ -1,56 +1,28 @@
 package co.edu.unbosque.model.persistence;
 
 import net.sourceforge.jFuzzyLogic.FIS;
-import net.sourceforge.jFuzzyLogic.rule.RuleBlock;
+
+import java.io.File;
 
 public class FclFileManager {
 
+    private final File file = new File("static" + File.separator + "road_system.fcl");
+
     private FIS fis;
 
-    public void loadFclFile(String filePath) {
-        fis = FIS.load(filePath, true);
-    }
-
-    public void setInputVariable(String keyName, double value) {
-        if (fis != null) {
-            fis.setVariable(keyName, value);
+    public void loadFclFile() {
+        String path = file.getPath();
+        try {
+            fis = FIS.load(path, true);
+            if (fis == null) {
+                throw new IllegalArgumentException("Error loading FCL file: " + path);
+            }
+        } catch (NullPointerException e) {
+            e.getMessage();
         }
     }
 
-    public void evaluate() {
-        if (fis != null) {
-            fis.evaluate();
-        }
-    }
-
-    public double getOutputVariable(String variableName) {
-        if (fis != null) {
-            return fis.getVariable(variableName).getValue();
-        }
-        return Double.NaN;
-
-        // Método para mostrar funciones de pertenencia
-//    public void showFuzzyChart() {
-//        if (fis != null) {
-//            fis.showFuzzyChart();
-//        }
-//    }
-
-        // Método para mostrar la visualización de las reglas
-//    public void showRuleBlockChart() {
-//        if (fis != null) {
-//            RuleBlock ruleBlock = fis.getRuleBlock(null); // null para obtener el primer RuleBlock
-//            if (ruleBlock != null) {
-//                ruleBlock.chart();
-//            }
-//        }
-//    }
-
-        // Método para mostrar la visualización del resultado
-//    public void showOutputChart() {
-//        if (fis != null) {
-//            fis.chart();
-//        }
-//    }
+    public FIS getFis() {
+        return fis;
     }
 }
